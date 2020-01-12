6d6382bc29f75a702567a438dd52ba1115a617f4
Marian Meres <marian@meres.sk>
2020-01-12 15:37:45 +0100

    docs

--- src/string/mm-parse-boolean.test.ts
+++ src/string/mm-parse-boolean.test.ts
@@ -2,15 +2,16 @@ import { mmParseBoolean } from './mm-parse-boolean';
 
 test('mmParseBoolean works', () => {
 
-    // undefs/null
+    // undefs/null/NaN (FALSE)
     expect(mmParseBoolean(undefined)).toEqual(false);
     expect(mmParseBoolean(null)).toEqual(false);
+    expect(mmParseBoolean(NaN)).toEqual(false);
 
     // booleans
     expect(mmParseBoolean(true)).toEqual(true);
     expect(mmParseBoolean(false)).toEqual(false);
 
-    // ints/floats
+    // ints/floats (TRUE)
     expect(mmParseBoolean(1)).toEqual(true);
     expect(mmParseBoolean(-1)).toEqual(true);
     expect(mmParseBoolean(2)).toEqual(true);
@@ -20,11 +21,11 @@ test('mmParseBoolean works', () => {
     expect(mmParseBoolean(0.123)).toEqual(true);
     expect(mmParseBoolean(-0.123)).toEqual(true);
 
+    // ints/floats (FALSE)
     expect(mmParseBoolean(0)).toEqual(false);
     expect(mmParseBoolean(0.00)).toEqual(false);
-    expect(mmParseBoolean(NaN)).toEqual(false);
 
-    // ints/floats written as strings
+    // ints/floats written as strings (TRUE)
     expect(mmParseBoolean('1')).toEqual(true);
     expect(mmParseBoolean('-1')).toEqual(true);
     expect(mmParseBoolean('2')).toEqual(true);
@@ -33,15 +34,13 @@ test('mmParseBoolean works', () => {
     expect(mmParseBoolean('2.345')).toEqual(true);
     expect(mmParseBoolean('0.123')).toEqual(true);
     expect(mmParseBoolean('-0.123')).toEqual(true);
-
-    expect(mmParseBoolean('0')).toEqual(false);
-    expect(mmParseBoolean('0.00')).toEqual(false);
     expect(mmParseBoolean(Math.random().toString())).toEqual(true);
 
-    // strings
-    expect(mmParseBoolean('')).toEqual(false);
+    // ints/floats written as strings (FALSE)
+    expect(mmParseBoolean('0')).toEqual(false);
+    expect(mmParseBoolean('0.00')).toEqual(false);
 
-    // true
+    // only few whitelisted strings are TRUE
     expect(mmParseBoolean('1')).toEqual(true);
     expect(mmParseBoolean('oN')).toEqual(true);
     expect(mmParseBoolean('tRUe')).toEqual(true);
@@ -51,12 +50,14 @@ test('mmParseBoolean works', () => {
     expect(mmParseBoolean('y')).toEqual(true);
 
     // every other string is false
+    expect(mmParseBoolean('')).toEqual(false);
     expect(mmParseBoolean('   ')).toEqual(false);
-    expect(mmParseBoolean('aasdf')).toEqual(false);
+    expect(mmParseBoolean('asdf')).toEqual(false);
     expect(mmParseBoolean('OFF')).toEqual(false);
     expect(mmParseBoolean('diSAbled')).toEqual(false);
+    expect(mmParseBoolean('xyz' + Math.random().toString())).toEqual(false);
 
-    // anything else is truthy
+    // anything else is truthy (but shouldn't be used here)
     expect(mmParseBoolean({})).toEqual(true);
     expect(mmParseBoolean(new Date)).toEqual(true);
     expect(mmParseBoolean(new RegExp('foo'))).toEqual(true);
